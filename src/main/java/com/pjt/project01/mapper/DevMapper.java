0bf85badfc8f8b763407f0028963c1b350c40ba2
yumi <cym052418@gmail.com>
2024-08-02 11:15:10 +0900

    부트

---
+++ src/main/java/com/pjt/project01/mapper/DevMapper.java
@@ -0,0 +1,47 @@
+package com.pjt.project01.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface DevMapper {
+
+	@Select("SELECT * FROM signup")
+	public List<Map> signup();
+	
+	@Select("SELECT * FROM StoreList")
+	public List<Map> storeList();
+	
+	@Insert(  "INSERT INTO review ("
+			+ "storeNum,"
+			+ "review"
+			+ ")VALUES ("
+			+ "#{storeNum},#{review}) ")
+	public int review(Map map);
+	
+	@Insert(  "INSERT INTO signup ("
+			+ "UserNum,"
+			+ "UserId,"
+			+ "UserPw,"
+			+ "UserName)VALUES "
+			+ "(#{UserId},#{UserPw},#{UserName}) ")
+	public int insertOne();
+		
+	@Select("SELECT A.* , B.Review "
+			+ "FROM StoreList as A "
+			+ "INNER JOIN Review as B "
+			+ "ON A.storeNum = B.storeNum")
+	public List<Map> list1();
+	
+	
+	
+	
+	
+	
+	
+	
+}
